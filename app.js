0a91f6eca45a07166bb12aa51751e804a49e1b5c
Júlia Rodríguez <juliette.ot5@gmail.com>
2020-05-28 19:06:00 +0200

    design

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 let rocketListController = new RocketListController(new RocketListModel(), new RocketControllerView());
 let rocket1 = rocketListController.createRocket("32WESSDS");
 let rocket2 = rocketListController.createRocket("LDSFJA32");
-rocketListController.handleShowRocket(rocket1.id);
-rocketListController.handleShowRocket(rocket2.id);
-rocketListController.handleShowRocketList();
-rocketListController.handleShowCurrentPower(rocket1.id);
-rocketListController.handleShowCurrentPower(rocket2.id);
+// rocketListController.handleShowRocket(rocket1.id);
+// rocketListController.handleShowRocket(rocket2.id);
+// rocketListController.handleShowRocketList();
+// rocketListController.handleShowCurrentPower(rocket1.id);
+// rocketListController.handleShowCurrentPower(rocket2.id);
 // for (let i = 0; i < 8; i++) {
 //     rocketListController.handleAccelerateRocket(rocket1.id);
 //     rocketListController.handleShowCurrentPower(rocket1.id);
--- app.ts
+++ app.ts
@@ -5,11 +5,11 @@ let rocketListController: RocketListController = new RocketListController(new Ro
 let rocket1: Rocket = rocketListController.createRocket("32WESSDS");
 let rocket2: Rocket = rocketListController.createRocket("LDSFJA32");
 
-rocketListController.handleShowRocket(rocket1.id);
-rocketListController.handleShowRocket(rocket2.id);
-rocketListController.handleShowRocketList();
-rocketListController.handleShowCurrentPower(rocket1.id);
-rocketListController.handleShowCurrentPower(rocket2.id);
+// rocketListController.handleShowRocket(rocket1.id);
+// rocketListController.handleShowRocket(rocket2.id);
+// rocketListController.handleShowRocketList();
+// rocketListController.handleShowCurrentPower(rocket1.id);
+// rocketListController.handleShowCurrentPower(rocket2.id);
 
 // for (let i = 0; i < 8; i++) {
 //     rocketListController.handleAccelerateRocket(rocket1.id);
